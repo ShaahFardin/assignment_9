2e2e8aceccb48d198858bef9b2ecf53125bd7f79
shaahfardin <fardinshaah@gmail.com>
2022-10-12 00:53:35 +0600

    Major changes in home page

--- src/component/Topic/Topic.js
+++ src/component/Topic/Topic.js
@@ -5,7 +5,7 @@ const Topic = ({ topic }) => {
   const { id, name, total, logo } = topic;
   return (
     <div>
-      <div className="border  bg-sky-600 text-white rounded-md shadow-lg p-10 md:flex items-center">
+      <div className="border  bg-sky-600 text-white rounded-md p-10 md:flex items-center">
         <img className="w-40 h-40" src={logo} alt="" />
         <div className="text-start text-xl leading-8 md:ml-10">
           <p>
--- src/component/Topics/Topics.js
+++ src/component/Topics/Topics.js
@@ -1,25 +1,21 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import Topic from "../Topic/Topic";
+import './Topics.css'
 
 const Topics = () => {
   const loadTopics = useLoaderData();
   const topics = loadTopics.data;
   console.log(topics);
   return (
-    <div>
+    <div className=" bg-green-400 forest">
       <div>
-        <div className="">
-          <img
-            src="https://img.freepik.com/free-vector/savannah-with-acacia-tree-green-grass-bushes-road-mountains-horizon-concept-travel-safari-trip-vector-cartoon-illustration-african-savanna-landscape_107791-8962.jpg?w=1380&t=st=1665497778~exp=1665498378~hmac=b7fde9f2dbecd5c62973c51990c89c6ac135d46d5672cff3d9186dc7de9a264b"
-            alt=""
-            className="w-full h-full bg-no-repeat bg-cover bg-fixed"
-          />
-          <h1 className="text-start text-slate-500 ml-20 text-5xl mt-20">
-            Take the QuizMaster quiz test and test your knowledge
+        <div>
+          <h1 className="text-start font-semibold text-slate-600 ml-20 text-5xl pt-20 ">
+            Take the QuizMaster quiz test !
           </h1>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 my-20 mx-20 ">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-16  mx-20 pb-20">
           {topics.map((topic) => (
             <Topic key={topic.id} topic={topic}></Topic>
           ))}
