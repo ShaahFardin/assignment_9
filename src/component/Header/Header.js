93dfed391ec0bf42b3e302029784fb6f9eff737f
shaahfardin <fardinshaah@gmail.com>
2022-10-11 11:44:32 +0600

    Header added

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <div>QuizMaster</div>
-            <div>
+        <div className='flex justify-between p-5 bg-sky-900 text-white'>
+            <div className='ml-10 text-2xl'>QuizMaster</div>
+            <div className='menu mr-10 '>
                 <Link to='/topics'>Topics</Link>
                 <Link to='/statistics'>Statistics</Link>
                 <Link to='/blog'>Blog</Link>
